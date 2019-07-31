2810552acac4fe9fb61e4211f939891335976b20
Will Hodgson <will.hodgson@current-rms.com>
2019-07-31 10:23:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       if holiday == :winter
-
+        all_supplies << supplies
       end
     end
   end

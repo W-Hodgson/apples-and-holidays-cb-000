68d1359554a9951fc18da4d93eb29b3b9803cee7
Will Hodgson <will.hodgson@current-rms.com>
2019-07-31 10:36:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       if supplies.any? {|supply| supply == "BBQ"}
-
+        holiday_names << holiday
       end
     end
   end
